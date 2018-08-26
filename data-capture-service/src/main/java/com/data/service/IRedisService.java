17119e328626d744080cfe862d986f753b7ec159
AlexHoang <1109504613@qq.com>
2018-08-27 00:17:53 +0800

    添加注释 -- alex

--- data-capture-service/src/main/java/com/data/service/IRedisService.java
+++ data-capture-service/src/main/java/com/data/service/IRedisService.java
@@ -9,12 +9,29 @@ import com.data.bean.User;
  */
 public interface IRedisService {
 	
+	/**
+	 * 保存用户信息
+	 * @param user
+	 */
 	void saveUserModel(User user);
 	
+	/**
+	 * 取出用户信息
+	 * @param workNo
+	 * @return
+	 */
 	User getUserModel(String workNo);
 	
+	/**
+	 * 更新用户信息
+	 * @param user
+	 */
 	void updateUserModel(User user);
 	
+	/**
+	 * 删除用户信息
+	 * @param workNo
+	 */
 	void deleteUserModel(String workNo);
 
 }
