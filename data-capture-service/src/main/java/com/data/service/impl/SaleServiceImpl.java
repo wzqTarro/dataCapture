bfbc5fc5b48e338a9c3426d39a3b26db3aef8b64
AlexHoang <1109504613@qq.com>
2018-10-03 02:17:11 +0800

    删包 -- alex

--- data-capture-service/src/main/java/com/data/service/impl/SaleServiceImpl.java
+++ data-capture-service/src/main/java/com/data/service/impl/SaleServiceImpl.java
@@ -22,7 +22,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.data.bean.Reject;
 import com.data.bean.Sale;
 import com.data.bean.TemplateProduct;
 import com.data.bean.TemplateStore;
@@ -32,7 +31,6 @@ import com.data.constant.WebConstant;
 import com.data.constant.dbSql.InsertId;
 import com.data.constant.dbSql.QueryId;
 import com.data.constant.enums.CodeEnum;
-import com.data.constant.enums.OrderEnum;
 import com.data.constant.enums.SaleEnum;
 import com.data.constant.enums.TipsEnum;
 import com.data.dto.CommonDTO;
