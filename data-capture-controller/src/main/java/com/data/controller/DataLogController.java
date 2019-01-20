24da71922e7fc99eb99a1b887cb2577c05069e14
AlexHoang <1109504613@qq.com>
2019-01-20 22:40:42 +0800

    添加swagger注解标识 -- alex

--- data-capture-controller/src/main/java/com/data/controller/DataLogController.java
+++ data-capture-controller/src/main/java/com/data/controller/DataLogController.java
@@ -3,7 +3,9 @@ package com.data.controller;
 import com.data.bean.DataLog;
 import com.data.service.IDataLogService;
 import com.data.utils.ResultUtil;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/dataLog")
+@CrossOrigin(origins="*", maxAge=3600)
 public class DataLogController {
 
     @Autowired
     private IDataLogService dataLogService;
 
     /**
-     * 多田间查询日志列表
+     * 多条件查询日志列表
      * @param dataLog
      * @param page
      * @param limit
@@ -28,6 +31,7 @@ public class DataLogController {
      * @throws Exception
      */
     @RequestMapping(value = "/queryDataLogByCondition", method = RequestMethod.GET)
+    @ApiOperation(value = "多条件查询日志列表", httpMethod = "GET")
     public ResultUtil queryDataLogByCondition(DataLog dataLog, String page, String limit) throws Exception {
         return dataLogService.queryDataLogByCondition(dataLog, page, limit);
     }
